3756ecbc67f610263153e490aec16105916968c2
Tomas Mc Phillips <tomas.mc-phillips@ucdconnect.ie>
2019-05-14 22:55:40 +0100

    Update gmail_collect.py

--- python/gmail_collect.py
+++ python/gmail_collect.py
@@ -14,16 +14,12 @@ import xml_parse
 class Gmail():
 
   def create_service(self):
-    # If modifying these scopes, delete the file token.pickle.
     SCOPES = ['https://www.googleapis.com/auth/gmail.readonly','https://www.googleapis.com/auth/gmail.modify']
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+  
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -81,17 +77,7 @@ class Gmail():
           exit()
 
   def GetMimeMessage(self,service, user_id, msg_id):
-    """Get a Message with given ID.
 
-    Args:
-      service: Authorized Gmail API service instance.
-      user_id: User's email address. The special value "me"
-      can be used to indicate the authenticated user.
-      msg_id: The ID of the Message required.
-
-    Returns:
-      A Message in HTML RAW form.
-    """
     try:
       message = service.users().messages().get(userId=user_id, id=msg_id,format='raw').execute()
       msg_str = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
@@ -104,37 +90,14 @@ class Gmail():
       print('An error occurred:')
 
   def GetMessage(self,service, user_id, msg_id):
-    """Get a Message with given ID.
 
-    Args:
-      service: Authorized Gmail API service instance.
-      user_id: User's email address. The special value "me"
-      can be used to indicate the authenticated user.
-      msg_id: The ID of the Message required.
-
-    Returns:
-      A Message.
-    """
   
     message = service.users().messages().get(userId=user_id, id=msg_id).execute()
 
     return message
 
   def ListMessagesMatchingQuery(self,service, user_id, query=''):
-    """List all Messages of the user's mailbox matching the query.
 
-    Args:
-      service: Authorized Gmail API service instance.
-      user_id: User's email address. The special value "me"
-      can be used to indicate the authenticated user.
-      query: String used to filter messages returned.
-      Eg.- 'from:user@some_domain.com' for Messages from a particular sender.
-
-    Returns:
-      List of Messages that match the criteria of the query. Note that the
-      returned list contains Message IDs, you must use get with the
-      appropriate ID to get the details of a Message.
-    """
     try:
       response = service.users().messages().list(userId=user_id,
                                                 q=query).execute()
